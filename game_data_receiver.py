cb1d449092a1961db64e00c7f26412398856cdcc
Bitbunn <85596130+Bitbunn@users.noreply.github.com>
2024-08-27 10:51:26 +0200

    Update game_data_receiver.py

--- game_data_receiver.py
+++ game_data_receiver.py
@@ -18,4 +18,14 @@ class GameDataReceiver:
 
     def map_telemetry_data(self, raw_data):
         print(f"Mapping telemetry data: {raw_data}")
-        # Implement your mapping logic here
+        telemetry_data = {
+            "vehicle_pos": [raw_data[self.data_mapping["vehicle_pos"][i]] for i in range(3)],
+            "vehicle_speed": [raw_data[self.data_mapping["vehicle_speed"][i]] for i in range(3)],
+            "vehicle_accel": [raw_data[self.data_mapping["vehicle_accel"][i]] for i in range(3)],
+            "vehicle_ang_euler": [raw_data[self.data_mapping["vehicle_ang_euler"][i]] for i in range(3)],
+            "vehicle_ang_speed": [raw_data[self.data_mapping["vehicle_ang_speed"][i]] for i in range(3)],
+            "engine_rpm": raw_data[self.data_mapping["engine_rpm"]],
+            "susp_deflect": [raw_data[self.data_mapping["susp_deflect"][i]] for i in range(4)],
+        }
+        print(f"Mapped telemetry data: {telemetry_data}")
+        return telemetry_data
